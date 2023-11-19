fcd79bd43f2ab70066aeb76264241abd8c412b73
Praniyal Thapa <praniyalthapa100@gmail.com>
2023-11-19 22:41:50 +0545

    Basic setup for our airplane project done

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+const apiRoutes=require('./routes/index');
+const {PORT}=require('./config/serverConfig');
+const db=require('./models/index');
+const setupStartServer=()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true})); //to read the request body properly
+
+    app.use('/api',apiRoutes);
+      app.listen(PORT,()=>{
+        console.log(`Server started on ${PORT}`);
+        if(process.env.DB_SYNC){
+          db.sequelize.sync({alter:true});
+        }
+
+      });
+}
+setupStartServer();
